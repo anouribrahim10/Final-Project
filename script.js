f83c987b384463a928b6a0e094c798ff158d6460
Anour Ibrahim ibrahim <>
2023-01-25 00:40:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function jS() {
+  var jFirst = document.getElementById("fname").value;
+  var jLast = document.getElementById("lname").value;
+  var email = document.getElementById("email").value;
+  var birthday = document.getElementById("birthday").value;
+  alert("Nice to meet you " + jFirst + " " + jLast + " ");
+  
+}
